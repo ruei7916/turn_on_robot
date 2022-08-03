ec987517d2cd56141da2c47454cc835fca54d586
Ricky <ruei7916@gmail.com>
2022-08-03 13:10:05 +0000

    remove useless things

--- launch/test.py
+++ launch/test.py
@@ -4,24 +4,16 @@ from launch_ros.actions import Node
 def generate_launch_description():
     return LaunchDescription([
         Node(
-            package='turtlesim',
-            namespace='turtlesim1',
-            executable='turtlesim_node',
-            name='sim'
-        ),
-        Node(
-            package='turtlesim',
-            namespace='turtlesim2',
-            executable='turtlesim_node',
-            name='sim'
-        ),
-        Node(
-            package='turtlesim',
-            executable='mimic',
-            name='mimic',
+            package='turn_on_robot',
+            executable='base_node',
             remappings=[
-                ('/input/pose', '/turtlesim1/turtle1/pose'),
-                ('/output/cmd_vel', '/turtlesim2/turtle1/cmd_vel'),
+                ('/cmd_vel','/turtle1/cmd_vel')
             ]
+        ),
+        Node(
+            package='tf2_ros',
+            executable='static_transform_publisher',
+            name='map2odomTF',
+            arguments=["--frame-id", "map", "--child-frame-id", "odom"]
         )
     ])
