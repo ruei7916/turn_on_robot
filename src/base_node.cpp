208ee8e25a2ecfb68b52a6dfd6445918b64de76a
fred1108 <110779703+fred1108@users.noreply.github.com>
2022-08-21 00:49:55 +0800

    Update base_node.cpp

--- src/base_node.cpp
+++ src/base_node.cpp
@@ -5,7 +5,7 @@
 #include "serial/serial.h"
 #include "nav_msgs/msg/odometry.hpp"
 #include "tf2/LinearMath/Quaternion.h"
-#include "tf2_geometry_msgs/tf2_geometry_msgs.hpp"
+#include "tf2_geometry_msgs/tf2_geometry_msgs.h"
 #include "tf2_ros/transform_broadcaster.h"
 
 #include <chrono>
