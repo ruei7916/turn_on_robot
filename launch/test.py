602bd2cec002ee914be0984f8f01fcf32ea51880
Ricky <ruei7916@gmail.com>
2022-08-29 21:38:33 +0800

    modify launch file

--- launch/test.py
+++ launch/test.py
@@ -2,18 +2,23 @@ from launch import LaunchDescription
 from launch_ros.actions import Node
 
 def generate_launch_description():
-    return LaunchDescription([
+    ld = LaunchDescription()
+
+    ld.add_action( 
         Node(
             package='turn_on_robot',
             executable='base_node',
             remappings=[
                 ('/cmd_vel','/turtle1/cmd_vel')
             ]
-        ),
+        )
+    )
+    ld.add_action(
         Node(
             package='tf2_ros',
             executable='static_transform_publisher',
             name='map2odomTF',
             arguments=["0", "0", "0", "0", "0", "0", "map", "odom"]
         )
-    ])
+    )
+    return ld
