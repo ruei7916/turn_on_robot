3392b77931dce8b4cbf6b4151f8c10d3c60528d0
Ricky <ruei7916@gmail.com>
2022-08-30 20:54:36 +0800

    add imu

--- src/base_node.cpp
+++ src/base_node.cpp
@@ -7,7 +7,10 @@
 #include "tf2/LinearMath/Quaternion.h"
 #include "tf2_geometry_msgs/tf2_geometry_msgs.h"
 #include "tf2_ros/transform_broadcaster.h"
+#include "sensor_msgs/msg/imu.hpp"
+#include "ICM20948.h"
 
+#include <math.h>
 #include <chrono>
 #include <thread>
 #include <future>
@@ -18,11 +21,11 @@
 #define RECEIVE_DATA_SIZE    11          // The length of data sent by the lower machine to ROS
 
 // wheel radius
-#define wheel_radius (3.0/100) // unit: meter
+#define wheel_radius (3.0/100) // unit: m
 // half of the distance between front wheels
-#define wheel_spacing (19.5/2.0/100) // unit: meter
+#define wheel_spacing (19.5/2.0/100) // unit: m
 // half of the distance between front wheel and rear wheel
-#define axle_spacing (15.1/2.0/100) // unit: meter
+#define axle_spacing (15.1/2.0/100) // unit: m
 
 
 class BaseNode : public rclcpp::Node
@@ -32,12 +35,11 @@ class BaseNode : public rclcpp::Node
     :Node("base_node")
     {
       rclcpp::QoS qos(rclcpp::KeepLast(1));
-      this->declare_parameter<bool>("imu_enabled", false);
       cmd_vel_sub = this->create_subscription<geometry_msgs::msg::Twist>("cmd_vel", qos, std::bind(&BaseNode::cmd_vel_callback, this, std::placeholders::_1));
       odom_publisher = this->create_publisher<nav_msgs::msg::Odometry>("odom", rclcpp::SensorDataQoS());
       tf_broadcaster_ = std::make_unique<tf2_ros::TransformBroadcaster>(*this);
-      this->get_parameter("imu_enabled", imu_enabled);
-      // set up  serial connection
+      imu_publisher = this->create_publisher<sensor_msgs::msg::Imu>("imu", rclcpp::SensorDataQoS());
+      // set up serial connection with arduino uno
       try{
         arduino_serial.setPort("/dev/ttyACM0");
         arduino_serial.setBaudrate(38400);
@@ -47,19 +49,26 @@ class BaseNode : public rclcpp::Node
         arduino_serial.setDTR();
         arduino_serial.setRTS();
       }
-      
       catch(serial::IOException& e){
         RCLCPP_ERROR(this->get_logger(), e.what());
         RCLCPP_ERROR(this->get_logger(), "An error occurred while establishing serial connection with Arduino uno.");
       }
-      
+
+      // create a polling thread for receiving data from arduino uno and publishing topics 
       if(arduino_serial.isOpen()){
         RCLCPP_INFO(this->get_logger(), "arduino serial port opened");
         future_ = exit_signal_.get_future();
         poll_thread_ = std::thread(&BaseNode::pollThread, this);
       }
 
-      
+      // initialize IMU
+      imuInit(&enMotionSensorType);
+      if(IMU_EN_SENSOR_TYPE_ICM20948 == enMotionSensorType){
+        RCLCPP_INFO(this->get_logger(), "Motion sersor is ICM-20948");
+      }
+      else{
+        RCLCPP_INFO(this->get_logger(), "Motion sersor NULL");
+      }
     }
     ~BaseNode()
     {
@@ -98,7 +107,7 @@ class BaseNode : public rclcpp::Node
             if(t==FRAME_TAIL){
               // finish receiving a frame
               if(recv_data[9]==(recv_data[0]^recv_data[1]^recv_data[2]^recv_data[3]^recv_data[4]^recv_data[5]^recv_data[6]^recv_data[7]^recv_data[8])){
-                // received frame data correct
+                // received data frame correct
                 //RCLCPP_INFO(this->get_logger(), "data correct");
                 float _1 = (int16_t)((recv_data[1]<<8)|recv_data[2])/1000.0;
                 float _2 = (int16_t)((recv_data[3]<<8)|recv_data[4])/1000.0;
@@ -116,11 +125,11 @@ class BaseNode : public rclcpp::Node
                 px+=((x * cos(pz) - y * sin(pz)) * sampling_time); //Calculate the displacement in the X direction, unit: m 
                 py+=((x * sin(pz) + y * cos(pz)) * sampling_time); //Calculate the displacement in the Y direction, unit: m 
                 pz+=(z * sampling_time); //The angular displacement about the Z axis, in rad 
-                RCLCPP_INFO(this->get_logger(),"p %f %f %f",px,py,pz);
+                //RCLCPP_INFO(this->get_logger(),"p %f %f %f",px,py,pz);
 
                 tf2::Quaternion odom_quat;
                 odom_quat.setRPY(0,0,pz);
-
+                /*
                 geometry_msgs::msg::TransformStamped tfStamped;
                 tfStamped.header.stamp = this->now();
                 tfStamped.header.frame_id = "odom";
@@ -130,8 +139,7 @@ class BaseNode : public rclcpp::Node
                 tfStamped.transform.translation.z = 0.0;
                 tfStamped.transform.rotation = tf2::toMsg(odom_quat);
                 tf_broadcaster_->sendTransform(tfStamped);
-
-
+                */
                 nav_msgs::msg::Odometry odom;
                 odom.header.stamp = this->now();
                 odom.header.frame_id = "odom";
@@ -145,24 +153,39 @@ class BaseNode : public rclcpp::Node
                 odom.twist.twist.angular.z = z;
 
                 if(x== 0&&y== 0&&z== 0){
-                  //If the velocity is zero, it means that the error of the encoder will be relatively small, and the data of the encoder will be considered more reliable
-                  //如果velocity是零，说明编码器的误差会比较小，认为编码器数据更可靠
+                  //如果小車速度是零，說明編碼器的誤差會比較小，認為編碼器數據更可靠
                   memcpy(&odom.pose.covariance, odom_pose_covariance2, sizeof(odom_pose_covariance2)),
                   memcpy(&odom.twist.covariance, odom_twist_covariance2, sizeof(odom_twist_covariance2));
                 }
                 else{
-                  //If the velocity of the trolley is non-zero, considering the sliding error that may be brought by the encoder in motion, the data of IMU is considered to be more reliable
-                  //如果小车velocity非零，考虑到运动中编码器可能带来的滑动误差，认为imu的数据更可靠
+                  //如果小車速度非零，考慮到運動中編碼器可能帶來的滑動誤差，認為imu的數據更可靠
                   memcpy(&odom.pose.covariance, odom_pose_covariance, sizeof(odom_pose_covariance)),
                   memcpy(&odom.twist.covariance, odom_twist_covariance, sizeof(odom_twist_covariance));       
                 }
                 odom_publisher->publish(odom);
                 //RCLCPP_INFO(this->get_logger(), "odom publised");
-                if(imu_enabled){
-                  // publish imu data
-                  
-                }
-                
+
+                // publish imu data
+                imuDataGet( &stAngles, &stGyroRawData, &stAccelRawData, &stMagnRawData);
+                sensor_msgs::msg::Imu imu_data;
+                imu_data.header.stamp = this->now();
+                imu_data.header.frame_id = "imu_link";
+                tf2::Quaternion imu_quat;
+                imu_data.orientation = tf2::toMsg(imu_quat);
+                imu_data.orientation_covariance[0] = -1;
+                imu_data.angular_velocity.x = stGyroRawData.fZ;
+                imu_data.angular_velocity.y = -stGyroRawData.fX;
+                imu_data.angular_velocity.z = -stGyroRawData.fY;
+                imu_data.angular_velocity_covariance[0] = 1e-6;
+                imu_data.angular_velocity_covariance[4] = 1e-6;
+                imu_data.angular_velocity_covariance[8] = 1e-6;
+                imu_data.linear_acceleration.x = stAccelRawData.fZ;
+                imu_data.linear_acceleration.y = -stAccelRawData.fX;
+                imu_data.linear_acceleration.z = -stAccelRawData.fY;
+                imu_data.linear_acceleration_covariance[0] = 1e-6;
+                imu_data.linear_acceleration_covariance[4] = 1e-6;
+                imu_data.linear_acceleration_covariance[8] = 1e-6;
+                imu_publisher->publish(imu_data);
               }else{
                 RCLCPP_INFO(this->get_logger(), "data error");
               }
@@ -196,12 +219,9 @@ class BaseNode : public rclcpp::Node
       send_data[7] = send_data[0]^send_data[1]^send_data[2]^send_data[3]^send_data[4]^send_data[5]^send_data[6];
       send_data[8] = FRAME_TAIL;
 
-      try
-      {
+      try{
         arduino_serial.write(send_data, SEND_DATA_SIZE); // Send data to arduino uno via serial port 
-      }
-      catch (serial::IOException& e)
-      {
+      }catch (serial::IOException& e){
         RCLCPP_ERROR(this->get_logger(), e.what());
         RCLCPP_ERROR(this->get_logger(),"Unable to send data through serial port"); // If sending data fails, an error message is printed
       }
@@ -210,11 +230,16 @@ class BaseNode : public rclcpp::Node
     rclcpp::Publisher<nav_msgs::msg::Odometry>::SharedPtr odom_publisher;
     std::unique_ptr<tf2_ros::TransformBroadcaster> tf_broadcaster_;
     rclcpp::Subscription<geometry_msgs::msg::Twist>::SharedPtr cmd_vel_sub;
+    rclcpp::Publisher<sensor_msgs::msg::Imu>::SharedPtr imu_publisher;
     serial::Serial arduino_serial;
     std::thread poll_thread_;
     std::shared_future<void> future_;
     std::promise<void> exit_signal_;
-    bool imu_enabled;
+    IMU_EN_SENSOR_TYPE enMotionSensorType;
+    IMU_ST_ANGLES_DATA stAngles;
+    IMU_ST_SENSOR_DATA stGyroRawData;
+    IMU_ST_SENSOR_DATA stAccelRawData;
+    IMU_ST_SENSOR_DATA stMagnRawData;
 };
 
 int main(int argc, char * argv[])
