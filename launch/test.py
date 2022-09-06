3e9e6ae10bb1a6afb3eef0d703ed085435e5c391
fred1108 <110779703+fred1108@users.noreply.github.com>
2022-09-06 11:02:48 +0800

    Update test.py

--- launch/test.py
+++ launch/test.py
@@ -22,7 +22,7 @@ def generate_launch_description():
             package='tf2_ros',
             executable='static_transform_publisher',
             name='imu_tf',
-            arguments=["0", "0", "0", "0", "0", "0", "base_link", "imu_link"]
+            arguments=["0.1", "0", "0", "0", "0", "0", "base_link", "imu_link"]
         )
     )
     ld.add_action(
