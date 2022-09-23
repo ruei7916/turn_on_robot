c2429e6b909ccbff9f8058074479dc84e270c514
ruei7916 <56112123+ruei7916@users.noreply.github.com>
2022-09-23 15:30:41 +0800

    remove imu tf amd cmd_vel remapping

--- launch/test.py
+++ launch/test.py
@@ -8,23 +8,6 @@ def generate_launch_description():
     pkg_share = launch_ros.substitutions.FindPackageShare(package='turn_on_robot').find('turn_on_robot')
     ld = launch.LaunchDescription()
 
-    ld.add_action( 
-        Node(
-            package='turn_on_robot',
-            executable='base_node',
-            remappings=[
-                ('/cmd_vel','/turtle1/cmd_vel')
-            ]
-        )
-    )
-    ld.add_action(
-        Node(
-            package='tf2_ros',
-            executable='static_transform_publisher',
-            name='imu_tf',
-            arguments=["0.1", "0", "0", "0", "0", "0", "base_link", "imu_link"]
-        )
-    )
     ld.add_action(
         Node(
             package='robot_localization',
