a43d328219623f053bc3279f428e53dd1aae00b5
ruei7916 <56112123+ruei7916@users.noreply.github.com>
2022-09-25 23:27:00 +0800

    Update base_node.h

--- include/turn_on_robot/base_node.h
+++ include/turn_on_robot/base_node.h
@@ -4,7 +4,7 @@ const double odom_pose_covariance[36]   = {1e-3,    0,    0,   0,   0,    0,
                                               0,    0,  1e6,   0,   0,    0,
                                               0,    0,    0, 1e6,   0,    0,
                                               0,    0,    0,   0, 1e6,    0,
-                                              0,    0,    0,   0,   0,  1e-3 };
+                                              0,    0,    0,   0,   0,  1e3 };
 
 const double odom_pose_covariance2[36]  = {1e-9,    0,    0,   0,   0,    0, 
                                               0, 1e-9,    0,   0,   0,    0,
@@ -18,7 +18,7 @@ const double odom_twist_covariance[36]  = {1e-3,    0,    0,   0,   0,    0,
                                               0,    0,  1e6,   0,   0,    0,
                                               0,    0,    0, 1e6,   0,    0,
                                               0,    0,    0,   0, 1e6,    0,
-                                              0,    0,    0,   0,   0,  1e-3 };
+                                              0,    0,    0,   0,   0,  1e3 };
                                               
 const double odom_twist_covariance2[36] = {1e-9,    0,    0,   0,   0,    0, 
                                               0, 1e-9,    0,   0,   0,    0,
